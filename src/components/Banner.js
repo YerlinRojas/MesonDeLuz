8541f03aac267e9dc13d5e966ad7bbbcf3d6be3e
Yerlin ROJAS <yerlinrojas808@gmail.com>
2023-08-10 15:28:31 -0300

    cambio de img a headerimg

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,12 +5,13 @@ const Banner = () => {
 return (
   <Link className='nn' to="https://workspace.google.com/intl/es-419_mx/products/calendar/"> 
    <div className='bannerZ'>
-  <img className='bannerImg' src="../img/logoElMESONdeluz_nueva_imagen-02.png" class="img-fluid " alt="VistaResto" ></img>
+  <img className='bannerImg' src="../img/MesondeLuz_Header.png" class="img-fluid " alt="VistaResto" width="300px" ></img>
   </div>
   </Link>
  
   )
 }
+
 
 export default Banner
 
